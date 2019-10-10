30cb0400a01b27d3bb73786469ba199a05eb76b2
Jason <liang.j4son@gmail.com>
2019-10-09 22:57:25 -0700

    Added Query

--- Project/src/Query.java
+++ Project/src/Query.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
 /**
  * A query which stores...
  *
@@ -5,6 +9,41 @@
  * @version v2.0.0
  */
 public class Query {
+	private Map<String, Set<SearchResult>> queries;
 
 
+	public Query() {
+		queries = new TreeMap<>();
+	}
+
+
+	/**
+	 * Search results
+	 */
+	private class SearchResult implements Comparable<SearchResult> {
+		private String search;
+		private String where;
+		private Long count;
+		private Double score;
+
+		public SearchResult(String search, String where, Long count, Double score) {
+			this.search = search;
+			this.where = where;
+			this.count = count;
+			this.score = score;
+		}
+
+		@Override
+		public int compareTo(SearchResult other) {
+			int temp;
+			if ((temp = Double.compare(this.score, other.score)) == 0) {
+				if ((temp = Long.compare(this.count, other.count)) == 0) {
+					if ((temp = this.where.compareTo(other.where)) == 0) {
+						return 0;
+					}
+				}
+			}
+			return temp;
+		}
+	}
 }
