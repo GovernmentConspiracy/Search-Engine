13bcb33e944b91cf963dba693c2f542e79697777
Jason <liang.j4son@gmail.com>
2019-10-17 16:49:32 -0700

    Merge branch 'master' of /Users/Jason/Dropbox/Java/USF/CS 212/Project/project-GovernmentConspiracy with conflicts.

--- Project/src/InvertedIndexBuilder.java
+++ Project/src/InvertedIndexBuilder.java
@@ -1,27 +1,16 @@
-import opennlp.tools.stemmer.Stemmer;
-import opennlp.tools.stemmer.snowball.SnowballStemmer;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
-
+//TODO refactor whole builder class to accept Query
 /**
  * A builder class for inverted index, which is an index to store words and
  * the location (both file location and position in file) of where those words were found.
  *
  * @author Jason Liang
- * @version v1.2.0
+ * @version v1.1.0
  */
 public class InvertedIndexBuilder {
-	/**
-	 * Default SnowballStemmer algorithm from OpenNLP.
-	 */
-	private static final SnowballStemmer.ALGORITHM DEFAULT_LANG = SnowballStemmer.ALGORITHM.ENGLISH;
-
 	/**
 	 * An index to store words and the location (both file location and position in file) of where those words were found.
 	 */
@@ -73,18 +62,7 @@ public class InvertedIndexBuilder {
 	 * @throws IOException if the files could not be inserted
 	 */
 	public InvertedIndexBuilder addFile(Path input) throws IOException {
-		Stemmer stemmer = new SnowballStemmer(DEFAULT_LANG);
-		try (
-				BufferedReader reader = Files.newBufferedReader(input, StandardCharsets.UTF_8)
-		) {
-			String line;
-			long i = 0;
-			while ((line = reader.readLine()) != null) {
-				for (String word : TextParser.parse(line)) {
-					index.indexPut(stemmer.stem(word).toString(), input.toString(), ++i);
-				}
-			}
-		}
+		index.index(input);
 		return this;
 	}
 
@@ -96,7 +74,7 @@ public class InvertedIndexBuilder {
 	public InvertedIndexBuilder transverse(Path input) throws IOException {
 		List<Path> paths = getFiles(input);
 		for (Path in : paths) {
-			addFile(in);
+			index.index(in);
 		}
 		return this;
 	}
@@ -134,5 +112,25 @@ public class InvertedIndexBuilder {
 	 */
 	public InvertedIndex getIndex() {
 		return index;
+	}
+
+	/**
+	 * Generates a JSON text file of the inverted index, stored at Path output
+	 *
+	 * @param output The output path to store the JSON object
+	 * @throws IOException if the output file could not be created or written
+	 */
+	public void indexToJSON(Path output) throws IOException {
+		index.indexToJSON(output);
+	}
+
+	/**
+	 * Generates a JSON text file of the count of words, stored at Path output
+	 *
+	 * @param output The output path to store the JSON object
+	 * @throws IOException if the output file could not be created or written
+	 */
+	public void countToJSON(Path output) throws IOException {
+		index.countToJSON(output);
 	}
 }
